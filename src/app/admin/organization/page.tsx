24ce46f9f08f53480b5987d28858c57a1ab2cf3a
Jim Wilson <jawilson1947@gmail.com>
2025-12-21 10:38:19 -0400

    Increased organization full address to width of page

--- src/app/admin/organization/page.tsx
+++ src/app/admin/organization/page.tsx
@@ -293,7 +293,7 @@ export default function OrganizationPage() {
                                 />
                             </div>
 
-                            <div className="col-span-1">
+                            <div className="col-span-1 md:col-span-2">
                                 <label className="block text-xs font-bold text-slate-700 mb-1 text-center">Full Address</label>
                                 <textarea
                                     className="w-full border border-slate-300 rounded-lg p-1.5 text-sm focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition-all h-32 resize-none text-center"
